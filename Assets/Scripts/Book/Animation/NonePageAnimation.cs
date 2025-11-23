d40b03a1f41dda34cb43779cfa17fd711dc0391d
Kamila Stawarska <kamila.stawarska@gmail.com>
2025-11-23 17:09:55 +0100

    Added none page animation

---
+++ Assets/Scripts/Book/Animation/NonePageAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using KBCore.Refs;
+using UnityEngine;
+
+namespace Book.Animation
+{
+    public class NonePageAnimation : MonoBehaviour, ISwapPageAnimation
+    {
+        public Action<FlipMode> OnPageSwapped { get; set; }
+        
+        [SerializeField, Self] private RectTransform bookPanel;
+        [SerializeField] private float pageFlipTime;
+        private RectTransform currentSpawnedPage;
+        private Page nextPage;
+        
+        public void FlipRightPage()
+        {
+            
+        }
+
+        public void FlipLeftPage()
+        {
+            
+        }
+
+        public void FlipPage(Page next)
+        {
+            if(currentSpawnedPage)
+                Destroy(currentSpawnedPage.gameObject);
+            
+            nextPage = next;
+            currentSpawnedPage = Instantiate(nextPage, bookPanel).GetComponent<RectTransform>();
+        }
+
+        private void OnValidate() => this.ValidateRefs();
+    }
+}
